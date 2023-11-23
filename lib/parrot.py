00ca3738d04cfb31e532abd167ccbe1a0fa0e33d
bill <bill.arnold@student.moringaschool.com>
2023-11-23 12:26:50 +0300

    complete

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,6 @@
-def parrot():
+def parrot(word="Squawk!"):
+    print(word)
+    return word
     pass
+print(parrot()) # prints "Squawk!" and returns "Squawk!"
+print(parrot("Hello!")) # prints "Hello!" and returns "Hello!"
